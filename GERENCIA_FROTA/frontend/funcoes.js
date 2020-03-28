a53243a36d6280ecf1aa26272476cca49275c7d6
Jadson Rodrigues <48611984+JadsDevolper@users.noreply.github.com>
2020-03-28 18:30:03 -0300

    Add files via upload

---
+++ GERENCIA_FROTA/frontend/funcoes.js
@@ -0,0 +1,21 @@
+
+
+function voltar(){
+    window.close("menu_inicial.html")
+}
+
+function clicar_Motorista(){
+    window.open("tela_motorista.html")
+}
+
+function clicar_Frota(){
+    window.open("tela_veiculo.html")
+}
+
+function sair() {
+    window.close("tela_login.html")
+}
+
+function login(){
+    window.open("menu_inicial.html")
+}
